f7dfaa79642ef8aa652e52e4f294986106ced15e
Learnr4Namish <namishkumar000@gmail.com>
2022-08-05 17:36:08 +0530

    make it better

--- server.js
+++ server.js
@@ -26,17 +26,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //const requestData = req.body;
 app.get('/', (req,res) => {
+    console.log(`PyChatRo: Requested Client IP address: ${req.ip}`);
     res.sendFile('index.html', { root: __dirname });
 });
 app.get('/newRoom', (req,res) => {
+    console.log(`PyChatRo: Requested Client IP address: ${req.ip}`);
     res.sendFile('2.html', { root: __dirname });
 });
 app.post('/createRoom', (req,res) => {
+    console.log(`PyChatRo: Requested Client IP address: ${req.ip}`);
     const bodyContent = req.body;
     const userName = bodyContent.userName + generateRoomID();
     const roomPassword = bodyContent.roomPassword;
     const roomID = generateRoomID();
     const db = getFirestore();
+    console.log(`PyChatRo: Accepted the request to create a room from a person named ${bodyContent.userName}`);
     const data = {
       [userName]:"I have successfully created this chat room!"
     };
@@ -87,14 +91,267 @@ app.post('/createRoom', (req,res) => {
      <p class="text-50">Successfully Created your chat-room. Please share the following details with the participants so that they can join.</p>
      <p class="text-50">Room ID: ${roomID}</p>
      <p class="text-50">Room Password: ${roomPassword}</p>
-     <p class="text-50">You can also share this link with them:-</p>
-     <p class="text-50">https://pychatro.herokuapp.com/join?roomID=${roomID}&roomPassword=${roomPassword}</p>
+     <form method="post" action="/joinRoom">
+     <input name="userRole" value="admin" hidden>
+     <input name="roomID" value="${roomID}" hidden>
+     <input name="roomPassword" value="${roomPassword}" hidden>
+     <input name="userName" value=${bodyContent.userName}" hidden>
+     <button type="submit" class="btn btn-primary mainBtn">Join my room</button>
+</form>
      </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 </html>
     `)
+    console.log(`PyChatRo: Successfully created the room by person named ${bodyContent.userName} with the room id ${roomID}`);
 });
 app.listen(process.env.PORT || 3000, ()=> {
     console.log("PyChatRo: Server started!")
+});
+app.post('/joinRoom', (req,res) => {
+    console.log(`PyChatRo: Requested Client IP address: ${req.ip}`);
+    const bodyContent = req.body;
+    const userRole = bodyContent.userRole;
+    const roomID = bodyContent.roomID;
+    const roomPassword = bodyContent.roomPassword;
+    const userName = bodyContent.userName;
+    console.log(bodyContent)
+    const db = getFirestore();
+    const roomRef = db.collection('rooms').doc(roomID).collection('auth').doc('password');
+    const doc = roomRef.get();
+    const doc2 = db.collection('rooms').doc(roomID).get();
+    if (doc2.exists) {
+        console.log(doc.data());
+        const actualRoomPassword = doc.data().password;
+        const passwordIsValid = roomPassword === actualRoomPassword;
+        if(!passwordIsValid) {
+          res.send (`
+          <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+            <title>PyChatRo | Chat on the go with full Security (Made by Namish Kumar)</title>
+        </head>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Open+Sans&family=Ubuntu&display=swap');
+            .mainBtn {
+                width: 10em; font-size:20px; padding: 10px; margin-left: 8px;
+            }
+            body {
+                font-family: Arial, sans-serif;
+                background-repeat:none;
+                height: 100%;
+                color: rgb(0, 0, 0);
+            }
+            html {
+                height: 100%;
+            }
+        </style>
+        <body>
+            <nav class="navbar navbar-expand-lg" style="color: rgb(0, 0, 0);">
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="#" style="font-size: 26.5px; color: rgb(0, 0, 0); font-weight: 500;">PyChatRo</a>
+                  <div class="navbar-toggler" style="border: none;" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="material-symbols-outlined">
+                        menu
+                        </span>
+                    </div>
+                </div>
+              </nav>
+             <div style="margin-left:8px; font-size:20px;">
+             <p class="text-50">We are sorry but the password that you have entered for the room id ${roomID} is invalid. Please enter valid password for this chat-room.</p>
+             <button type="button" class="btn btn-primary mainBtn" onclick="location.href = '/join'">Try Again</button>
+        </form>
+             </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+        </body>
+        </html>
+        `);
+        }else{
+          res.send (`
+          <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+            <title>PyChatRo | Chat on the go with full Security (Made by Namish Kumar)</title>
+        </head>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Open+Sans&family=Ubuntu&display=swap');
+            .mainBtn {
+                width: 10em; font-size:20px; padding: 10px; margin-left: 8px;
+            }
+            body {
+                font-family: Arial, sans-serif;
+                background-repeat:none;
+                height: 100%;
+                color: rgb(0, 0, 0);
+            }
+            html {
+                height: 100%;
+            }
+        </style>
+        <body>
+            <nav class="navbar navbar-expand-lg" style="color: rgb(0, 0, 0);">
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="#" style="font-size: 26.5px; color: rgb(0, 0, 0); font-weight: 500;">PyChatRo</a>
+                  <div class="navbar-toggler" style="border: none;" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="material-symbols-outlined">
+                        menu
+                        </span>
+                    </div>
+                </div>
+              </nav>
+             <div style="margin-left:8px; font-size:20px;">
+             <p class="text-50">Welcome to this chat room ${userName}! Please click on the button below to enter the chat room.</p>
+             <form method="post" action="/chat">
+             <input name="userRole" value=${userRole} hidden>
+       <input name="roomID" value="${roomID}" hidden>
+       <input name="roomPassword" value="${roomPassword}" hidden>
+       <input name="userName" value=${userName}" hidden>
+        </form>
+             </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+        </body>
+        </html>
+        `);
+        }
+    } else {
+      console.log(`PyChatRo: No such Chat Room called ${roomID}`);
+      res.send(`
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+          <title>PyChatRo | Chat on the go with full Security (Made by Namish Kumar)</title>
+      </head>
+      <style>
+          @import url('https://fonts.googleapis.com/css2?family=Open+Sans&family=Ubuntu&display=swap');
+          .mainBtn {
+              width: 10em; font-size:20px; padding: 10px; margin-left: 8px;
+          }
+          body {
+              font-family: Arial, sans-serif;
+              background-repeat:none;
+              height: 100%;
+              color: rgb(0, 0, 0);
+          }
+          html {
+              height: 100%;
+          }
+      </style>
+      <body>
+          <nav class="navbar navbar-expand-lg" style="color: rgb(0, 0, 0);">
+              <div class="container-fluid">
+                <a class="navbar-brand" href="#" style="font-size: 26.5px; color: rgb(0, 0, 0); font-weight: 500;">PyChatRo</a>
+                <div class="navbar-toggler" style="border: none;" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="material-symbols-outlined">
+                      menu
+                      </span>
+                  </div>
+              </div>
+            </nav>
+           <div style="margin-left:8px; font-size:20px;">
+           <p class="text-50">We are sorry but the room id ${roomID} is not a valid PyChatRo chat-room. It may have been expired or deleted by the admin. Consider creating a chat room if you want to.</p>
+           <button type="button" class="btn btn-primary mainBtn" onclick="location.href = '/newRoom'">Create my chat room</button>
+      </form>
+           </div>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+      </body>
+      </html>
+      `)
+    }
+});
+app.get('/join', (req,res) => {
+    console.log(`PyChatRo: Requested Client IP address: ${req.ip}`);
+    res.sendFile('3.html', { root: __dirname });
+});
+app.post('/chat', (req,res) => {
+    console.log(`PyChatRo: Requested Client IP address: ${req.ip}`);
+    const bodyContent = req.body;
+    const userRole = bodyContent.userRole;
+    const roomID = bodyContent.roomID;
+    const roomPassword = bodyContent.roomPassword;
+    const userName = bodyContent.userName;
+    if(userRole === "admin") {
+        res.send(
+        `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+            <title>PyChatRo | Chat on the go with full Security (Made by Namish Kumar)</title>
+        </head>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Open+Sans&family=Ubuntu&display=swap');
+            .mainBtn {
+                width: 10em; font-size:20px; padding: 10px; margin-left: 8px;
+            }
+            body {
+                font-family: Arial, sans-serif;
+                background-repeat:none;
+                height: 100%;
+                color: rgb(0, 0, 0);
+            }
+            html {
+                height: 100%;
+            }
+        </style>
+        <body>
+            <nav class="navbar navbar-expand-lg" style="color: rgb(0, 0, 0);">
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="#" style="font-size: 26.5px; color: rgb(0, 0, 0); font-weight: 500;">PyChatRo</a>
+                  <div class="navbar-toggler" style="border: none;" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="material-symbols-outlined">
+                        menu
+                        </span>
+                    </div>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+</div>
+                </div>
+              </nav>
+             <div style="margin-left:8px; font-size:20px;">
+             <div id="chatsContainer">
+
+             </div>
+             </div>
+             <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.0.0-alpha.1/axios.min.js" integrity="sha512-xIPqqrfvUAc/Cspuj7Bq0UtHNo/5qkdyngx6Vwt+tmbvTLDszzXM0G6c91LXmGrRx8KEPulT+AfOOez+TeVylg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+             <script>
+             axios.post('/readMessages', {
+                roomID: ${roomID},
+                roomPassword: ${roomPassword}
+              })
+              .then(function (response) {
+                console.log(response);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+             </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+        </body>
+        </html>`
+        );
+    }
+});
+app.post('/delete', (req,res) => {
+   
+});
+app.post('/readMessages', (req,res) => {
+   
 });
